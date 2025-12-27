3a06dc21609cf571305c2808c5d9cbe24427bbf9
mateomomazostristes-byte <mateomomazostristes@gmail.com>
2025-12-27 16:44:30 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,14 @@
 const productos = [
   {
-    nombre: "Organizador de SDs y USBs",
-    descripcion: "Mantén tus tarjetas y USBs ordenadas y a mano.",
-    categoria: "organizer" "sd" "usb",
-    imagen: "https://github.com/mateomomazostristes-byte/mateo3d-shop/blob/main/img/usb-sd-organizer.jpeg"
+    nombre: "Organizador de Tarjetas",
+    descripcion: "Mantén tus tarjetas ordenadas y a mano.",
+    categoria: "organizer",
+    imagen: "https://i.imgur.com/xyz123.png"
   },
   {
     nombre: "Soporte USB",
     descripcion: "Organiza tus USBs de manera elegante.",
     categoria: "usb",
     imagen: "https://i.imgur.com/abc456.png"
-  },
-  {
-    nombre: "Mini Estante SD",
-    descripcion: "Guarda tus tarjetas SD sin perder ninguna.",
-    categoria: "sd",
-    imagen: "https://i.imgur.com/def789.png"
   }
 ];
-
